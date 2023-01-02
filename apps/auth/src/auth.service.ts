4458c10aa4f81fd8a2d2bb4f4ae37bea84d07be1
Niklas Andersson <niklas.andersson@iths.se>
2023-01-02 13:13:04 +0100

    Add find create methods

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './db';
+import { AuthFrom } from '@app/shared/config';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findUserById(id: string, authFrom: AuthFrom) {
+    return authFrom === AuthFrom.GITHUB_ID
+      ? await this.userModel.findOne({ githubId: id }).exec()
+      : await this.userModel.findById(id).exec();
+  }
+
+  async findOrCreate(details: User, authFrom: AuthFrom) {
+    const user = await this.findUserById(details.githubId, authFrom);
+    if (user) return user;
+
+    return await this.createUser(details);
+  }
+
+  async createUser(details: User) {
+    return this.userModel.create(details);
   }
 }
