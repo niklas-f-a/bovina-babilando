95a0e3e3fc59e33914073db44e741730326c2cb2
Niklas Andersson <niklas.andersson@iths.se>
2023-01-02 13:27:20 +0100

    Add rabbit queue

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,27 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+
+  const rabbitOptions = app.get(ConfigService).get('rabbitOptions');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        `amqp://${rabbitOptions.user}:${rabbitOptions.pass}@${rabbitOptions.host}`,
+      ],
+      noAck: false,
+      queue: rabbitOptions.queue.auth,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
