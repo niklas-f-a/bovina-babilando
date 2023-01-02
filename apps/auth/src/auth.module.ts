51848995b7b71f939dc5583dd16d0af67b29745c
Niklas Andersson <niklas.andersson@iths.se>
2023-01-02 13:27:32 +0100

    Update Auth module

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,24 @@
+import { SharedModule } from '@app/shared';
+import { configuration } from '@app/shared/config';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { dbConnection, User, UserSchema } from './db';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './.env',
+      load: [configuration],
+    }),
+    dbConnection,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    SharedModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [{ provide: 'AUTH_SERVICE', useClass: AuthService }],
 })
 export class AuthModule {}
