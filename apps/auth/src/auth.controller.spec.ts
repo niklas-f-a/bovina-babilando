5511718b096bd94935db4608e241a657f81a1661
Niklas Andersson <niklas.andersson@iths.se>
2023-01-03 12:02:36 +0100

    change test

--- apps/auth/src/auth.controller.spec.ts
+++ apps/auth/src/auth.controller.spec.ts
@@ -1,32 +1,26 @@
 import { SharedModule } from '@app/shared';
-import { configuration, ServiceTokens } from '@app/shared/config';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ServiceTokens } from '@app/shared/config';
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { dbConnection, User, UserSchema } from './db';
 
 describe('AuthController', () => {
   let authController: AuthController;
 
+  const mockAuthService = {};
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: './.env',
-          load: [configuration],
-        }),
-        dbConnection,
-        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-        SharedModule,
-      ],
+      imports: [SharedModule],
       controllers: [AuthController],
       providers: [
         { provide: ServiceTokens.AUTH_SERVICE, useClass: AuthService },
       ],
-    }).compile();
+    })
+      .overrideProvider(ServiceTokens.AUTH_SERVICE)
+      .useValue(mockAuthService)
+      .compile();
 
     authController = app.get<AuthController>(AuthController);
   });
