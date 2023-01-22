5b4514e87b57e4b5be8cca2300a026d3e535006f
Niklas Andersson <niklas.andersson@iths.se>
2023-01-22 13:44:42 +0100

    rm commented out code

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -8,7 +8,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { AuthenticatedGuard, JwtAuthGuard } from '../../auth/src/guards';
+import { AuthenticatedGuard } from '../../auth/src/guards';
 import { IUser } from 'apps/user/src/db';
 import { catchError } from 'rxjs';
 import { User } from './decorators';
@@ -22,39 +22,6 @@ export class AppController {
     @Inject(ClientTokens.AUTH) private authClient: ClientProxy,
     @Inject(ClientTokens.USER) private userClient: ClientProxy,
   ) {}
-
-  // @Post('auth/login')
-  // async login(
-  //   @Session() session: Record<string, any>,
-  //   @Body() payload: LoginDto,
-  // ) {
-  //   const access_token = (await firstValueFrom(
-  //     this.authClient.send({ cmd: 'login' }, payload).pipe(
-  //       switchMap((res) => {
-  //         if (res.status === 403) {
-  //           throw new ForbiddenException({ ...res.response });
-  //         }
-  //         return of(res.access_token);
-  //       }),
-  //     ),
-  //   )) as string;
-
-  //   session['access_token'] = access_token;
-  //   return { message: 'ok' };
-  // }
-
-  // @Get('test-guard')
-  // testGuard(@Req() req: Request) {
-  //   console.log(req);
-
-  //   const access_token = 'token';
-  //   const record = new RmqRecordBuilder()
-  //     .setOptions({
-  //       headers: { ['token']: access_token },
-  //     })
-  //     .build();
-  //   return this.authClient.send({ cmd: 'status' }, record);
-  // }
 
   // DONT USE
   @Get('test')
