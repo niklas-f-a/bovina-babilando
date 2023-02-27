1272feb759c507da1d17fef351b0b51662c9bf74
Niklas Andersson <niklas.andersson@iths.se>
2023-02-27 18:24:40 +0100

    rm unused imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { ClientTokens, configuration, RabbitQueue } from '@app/shared/config';
 import { rabbitProvider } from '@app/shared/providers';
 import { SessionSerializer } from 'apps/api/src/serializer';
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_PIPE } from '@nestjs/core';
 import { PassportModule } from '@nestjs/passport';
 import { AppController } from './app.controller';
 import { GithubStrategy } from './strategies';
