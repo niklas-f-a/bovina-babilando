d5f9d3872a390b2c1f8cc540293b6f8c43dc8459
Niklas Andersson <niklas.andersson@iths.se>
2023-02-27 18:27:05 +0100

    Add session and adapter to chat

--- apps/chat/src/main.ts
+++ apps/chat/src/main.ts
@@ -2,11 +2,19 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ChatModule } from './chat.module';
+import { IOAdapter } from './IO.adabter';
+import * as session from 'express-session';
+import { makeMongo } from '@app/shared/providers';
 
 async function bootstrap() {
   const app = await NestFactory.create(ChatModule);
 
-  const rabbitOptions = app.get(ConfigService).get('rabbitOptions');
+  const configService = app.get(ConfigService);
+  const rabbitOptions = configService.get('rabbitOptions');
+  const mongoCred = configService.get('authDb');
+  const sessionCred = configService.get<{ collection: string; secret: string }>(
+    'session',
+  );
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
@@ -19,6 +27,22 @@ async function bootstrap() {
       },
     },
   });
+
+  const sess = session({
+    secret: sessionCred.secret,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: makeMongo({
+      uri: mongoCred.uri,
+      collection: sessionCred.collection,
+      session,
+    }),
+  });
+
+  app.useWebSocketAdapter(new IOAdapter(app, sess));
 
   app.startAllMicroservices();
   app.listen(5050);
